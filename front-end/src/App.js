c3132c890f42a330f0ce83d655c420213b3c89f9
Eva <evaliisagalen@gmail.com>
2018-01-30 13:31:21 -0800

    added routes and state for posts

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MyRoutes from './config/routes';
 
 class App extends Component {
   render() {
@@ -10,6 +11,7 @@ class App extends Component {
           <h1 className="App-title">reddit</h1>
           <h2 className="App-blurb">The front page of the web</h2>
         </header>
+        { MyRoutes }
       </div>
     );
   }
--- front-end/src/pages/HomePage.js
+++ front-end/src/pages/HomePage.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
 
 class HomePage extends Component {
+  constructor() {
+    super();
+    this.state = {
+      allPosts: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:8080/api/posts').then((res) => {
+      return res.json(); // res cannot be read, need to convert to json
+    }).then((json) => {
+      this.setState({ allPosts: json })
+    });
+  }
+
   render() {
     return (
-      <p className="HomePage">
-	      Home Page
-      </p>
+      <div>
+        {
+          this.state.allPosts.map((eachPost, idx) => {
+            return <div key={ idx }>
+                      <h2>{ eachPost.title }</h2>
+                      <p>{ eachPost.content }</p>
+                    </div>
+          })
+        }
+      </div>
     );
   }
 }
